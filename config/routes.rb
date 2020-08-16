ad3a84fbff0822c7933e24fa98b8383932cf66f7
Sittikorn <easy2dev@gmail.com>
2020-08-16 19:47:24 +0700

    add articles for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,37 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
+
+
+  ### Controller Namespaces and Routing
+  ## url: /admin/... --> folder: controller/admin/...
+  # namespace :admin do
+  #   resources :comments
+  # end
+  ## without /admin/
+  # scope module: 'admin' do
+  #   resources :articles, :comments
+  # end
+  ## or
+  # resources :articles, module: 'admin'
+  ## if you wanna route /admin/articles to ArticlesController (without the Admin::)
+  # scope '/admin' do
+  #   resources :articles, :comments
+  # end
+  ## or
+  # resources :articles, path: '/admin/articles'
+
+  # resources :photos do
+  #   member do
+  #     get 'search'
+  #   end
+  # end
+
+  # resources :photos do
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
+
 end
