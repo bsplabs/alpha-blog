40f74164553558400d501831ffd5f63789994650
Sittikorn <easy2dev@gmail.com>
2020-09-13 20:10:53 +0700

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
     def new 
         @user = User.new
     end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "คุณลงทะเบียนสำเร็จเเล้ว ยินดีต้อนรับ #{@user.username} เข้าสู่ Dark[CMD]"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
