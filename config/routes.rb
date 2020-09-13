0a1acd2492ea62dc05923315f8ac038ad70282de
Sittikorn <easy2dev@gmail.com>
2020-09-13 19:49:37 +0700

    Add users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 
   ### Controller Namespaces and Routing
   ## url: /admin/... --> folder: controller/admin/...
