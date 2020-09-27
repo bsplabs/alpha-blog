944b97e1516ea7e7cc601f486b23b2b33e0d01bb
Sittikorn <easy2dev@gmail.com>
2020-09-27 10:32:45 +0700

    add admin to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -37,6 +39,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "บัญชีและบทความของคุณทั้งหมดถูกลบเรียบร้อยเเล้ว"
+        redirect_to articles_path
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
@@ -44,5 +53,12 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user 
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "คุณสามารถแก้ไขได้เฉพาะบัญชีของคุณ"
+            redirect_to @user
+        end
     end
 end
